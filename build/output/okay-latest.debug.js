55c10e33562fb30e62b31a9163cb0356db1f97f1
Hunter Loftis <hunter+github@hunterloftis.com>
2011-06-28 11:06:40 -0400

    Added submit binding

--- build/output/okay-latest.debug.js
+++ build/output/okay-latest.debug.js
@@ -375,6 +375,30 @@ window['ok'] = window['ok'] || {};
   };
   
 })(ok);(function(ok) {
+
+  function SubmitBinding(node, callback, vm) {
+    this.node = node;
+    this.callback = callback;
+    this.vm = vm;
+    $(node).bind('submit', _.bind(this.activate, this));
+  }
+  SubmitBinding.prototype = {
+    activate: function(event) {
+      if (!this.callback.call(this.vm, [event])) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+    },
+    release: function() {
+      $(node).unbind('submit', this.callback);  // TODO: use the dom adapter
+    }
+  };
+  
+  ok.binding['submit'] = function(node, callback, vm) {
+    return new SubmitBinding(node, callback, vm);
+  };
+  
+})(ok);(function(ok) {
   
   function ValueBinding(node, subscribable) {
     this.node = node;
