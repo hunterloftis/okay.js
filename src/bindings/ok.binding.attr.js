9ea3f550e3f288e327b34c51ccdc5d4b4910cbd5
Hunter Loftis <hunter+github@hunterloftis.com>
2011-08-23 14:58:37 -0400

    Added an attr binding

---
+++ src/bindings/ok.binding.attr.js
@@ -0,0 +1,26 @@
+(function(ok) {
+  
+  function AttrBinding(node, attrName, subscribable) {
+    this.node = node;
+    this.attrName = attrName;
+    this.subscribable = subscribable;
+    ok.safeSubscribe(subscribable, this.update, this);
+  }
+  AttrBinding.prototype = {
+    update: function(newValue) {
+      $(this.node).attr(this.attrName, newValue);
+    },
+    release: function() {
+      this.subscribable.unsubscribe(this.update);
+    }
+  };
+  
+  ok.binding['attr'] = function(node, attributes, vm) {
+    var bindings = [];
+    for (var attrName in attributes) {
+      bindings.push(new AttrBinding(node, attrName, attributes[attrName]));  
+    };
+    return bindings;
+  };
+  
+})(ok);
